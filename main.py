ab7a6616c8b70f1449c2fdf3dc3479201abf22a2
sakuya10969 <sakuya.f20010618@gmail.com>
2025-10-06 16:43:13 +0900

    refactor

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 # 八村塁のplayer_idを取得
 player_dict = players.find_players_by_full_name("Rui Hachimura")
 rui_id = player_dict[0]['id']   # 八村のIDを取得
-
 # 2019-20から2024-25シーズンのログをまとめて取得
 seasons = ["2019-20", "2020-21", "2021-22", "2022-23", "2023-24","2024-25"]
 
@@ -15,10 +14,8 @@ for season in seasons:
     df = gamelog.get_data_frames()[0]
     df["SEASON"] = season
     all_games.append(df)
-
 # 結合
 games_df = pd.concat(all_games, ignore_index=True)
-
 # 必要な列だけ抽出（例：日付、対戦チーム、得点、リバウンド、アシスト、出場時間など）
 games_df = games_df[[
     "SEASON", "GAME_DATE", "MATCHUP", "WL", "MIN", "PTS",
